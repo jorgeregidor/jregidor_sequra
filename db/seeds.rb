2d8d8aeb61da4f8bf8129207b6d6a420ba0e8239
Jorge Regidor <jorgeregidor@gmail.com>
2024-10-08 15:39:51 +0200

    created Merchant seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Merchants seeds from csv
+require 'csv'
+
+CSV.foreach(Rails.root.join("db/seeds/csv/merchants.csv"), headers: true, col_sep: ';') do |row|
+  begin
+    date = Date.strptime(row['live_on'], '%Y-%m-%d')
+  rescue TypeError
+    puts "Invalid date format for #{row['live_on']}"
+    next
+  end
+
+  Merchant.create(
+    id: row['id'],
+    reference: row['reference'],
+    email: row['email'],
+    live_on: date,
+    disbursement_frequency: row['disbursement_frequency'],
+    disbursement_wday: date.wday,
+    minimum_monthly_fee_cents: (row['minimum_monthly_fee'].to_f * 100).to_i
+  )
+end
+p 'Created merchants'
