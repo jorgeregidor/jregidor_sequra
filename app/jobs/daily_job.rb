372a5861dc1c72296d80b7b796fa4c42ffe9ee8c
Jorge Regidor <jorgeregidor@gmail.com>
2024-10-10 15:40:08 +0200

    deleted development comment

--- app/jobs/daily_job.rb
+++ app/jobs/daily_job.rb
@@ -1,9 +1,9 @@
 class DailyJob < ApplicationJob
   queue_as :default
 
-  def perform(values={})
+  def perform(values = {})
     date = values[:date] ||= Date.today
-    # DailyDisbursementsService.new(date: date).call
-    puts "************ ejecuta ************* #{date}"
+
+    DailyDisbursementsService.new(date: date).call
   end
 end
