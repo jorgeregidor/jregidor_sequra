94ae7164ae6a9f9efafda4979ed5a648ecf05b19
Jorge Regidor <jorgeregidor@gmail.com>
2024-10-10 16:46:31 +0200

    created AllTotalMonthlyFeeService unit test

---
+++ spec/services/all_total_monthly_fee_service_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe AllTotalMonthlyFeeService, type: :service do
+  let(:date) { Date.new(2024, 10, 1) }
+  let!(:merchant1) { create(:merchant, reference: 'M123') }
+  let!(:merchant2) { create(:merchant, reference: 'M456') }
+  let(:double) { instance_double(TotalMonthlyFeeService) }
+
+
+  subject { AllTotalMonthlyFeeService.new(date: date) }
+
+  describe '#call' do
+    it 'calls TotalMonthlyFeeService for each merchant' do
+      expect(TotalMonthlyFeeService).to receive(:new).and_return(double).twice
+      expect(double).to receive(:call).twice
+
+      subject.call
+    end
+  end
+end
