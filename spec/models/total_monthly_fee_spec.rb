22f429d21bee0dc4b80513ff8488ec0e20add5c4
Jorge Regidor <jorgeregidor@gmail.com>
2024-10-10 15:42:43 +0200

    created TotalMonthlyFee model unit test

---
+++ spec/models/total_monthly_fee_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe TotalMonthlyFee, type: :model do
+  let(:merchant) { Merchant.create(reference: 'M001', disbursement_frequency: 'weekly', live_on: Date.today) }
+
+
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      total_monthly_fee = TotalMonthlyFee.new(
+        merchant_reference: merchant.reference,
+        date: Date.today,
+        amount_cents: 1000,
+        reached: true
+      )
+      expect(total_monthly_fee).to be_valid
+    end
+
+    it 'is not valid without a date' do
+      total_monthly_fee = TotalMonthlyFee.new(
+        merchant_reference: merchant.reference,
+        amount_cents: 1000,
+        reached: true
+      )
+      expect(total_monthly_fee).not_to be_valid
+      expect(total_monthly_fee.errors[:date]).to include("can't be blank")
+    end
+
+    it 'is not valid without reached' do
+      total_monthly_fee = TotalMonthlyFee.new(
+        merchant_reference: merchant.reference,
+        date: Date.today,
+        amount_cents: 1000
+      )
+      expect(total_monthly_fee).not_to be_valid
+      expect(total_monthly_fee.errors[:reached]).to include("can't be blank")
+    end
+  end
+end
